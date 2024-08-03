168503870306ae2e282a7a7986d038a48ae2992f
YunFy26 <207946431@qq.com>
2024-08-04 00:54:48 +0800

    自动生成网络拓扑

--- docker/GenNetwork.py
+++ docker/GenNetwork.py
@@ -3,7 +3,7 @@ from typing import List
 
 from docker.NetworkTopology import NetworkTopology, Router, Network, Switch, Host
 
-
+# TODO：按照gen_docker.py的逻辑，实现GenNetwork类，实现自动生成网络拓扑
 class GenNetwork:
     def __init__(self, networkTopology: NetworkTopology):
         self.networks:List[Network] = networkTopology.getNetworks
