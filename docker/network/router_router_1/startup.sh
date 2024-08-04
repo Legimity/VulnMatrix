6e445439e00bae9cfa534a11d9a27ef741209e31
YunFy26 <207946431@qq.com>
2024-08-05 00:14:47 +0800

    运行GenNetwork.py生成的网络拓扑节点

---
+++ docker/network/router_router_1/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sysctl -w net.ipv4.ip_forward=1
+for f in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 0 > "$f"; done
+tail -f /dev/null
--- docker/services/switch_dev_10_20_2_2/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sysctl -w net.ipv4.ip_forward=1
-for f in /proc/sys/net/ipv4/conf/*/rp_filter; do echo 0 > "$f"; done
-
-tail -f /dev/null
